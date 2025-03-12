5c518b42c53bf4b963a55b2e77c7a06c0ed286e5
shon0921 <0921shon@naver.com>
2025-03-12 11:34:45 +0900

    .

--- src/main/java/kopo/poly/controller/NoticeJoinController.java
+++ src/main/java/kopo/poly/controller/NoticeJoinController.java
@@ -111,4 +111,32 @@ public class NoticeJoinController {
         // templates/notice/noticeList.html
         return "notice/noticeListJoin";
     }
+
+    @GetMapping(value = "noticeListUsingQueryDSL")
+    public String noticeListUsingQueryDSL(HttpSession session, ModelMap model)
+            throws Exception {
+
+        // 로그 찍기(추후 찍은 로그를 통해 이 함수에 접근했는지 파악하기 용이하다.)
+        log.info(this.getClass().getName() + ".noticeListUsingQueryDSLStart!");
+
+        // 로그인된 사용자 아이디는 Session에 저장함
+        // 교육용으로 아직 로그인을 구현하지 않았기 때문에 Session에 데이터를 저장하지 않았음
+        // 추후 로그인을 구현할 것으로 가정하고, 공지사항 리스트 출력하는 함수에서 로그인 한 것처럼 Session 값을 생성함
+        session.setAttribute("SESSION_USER_ID","USER01");
+
+        // 공지사항 리스트 조회하기
+        // Java 8부터 제공되는 Optional 활용하여 NPE(Null Pointer Exception) 처리
+        List<NoticeDTO> rList = Optional.ofNullable(noticeJoinService.getNoticeListForQueryDSL())
+                .orElseGet(ArrayList::new);
+
+        //조회된 리스트 결과값 넣어주기
+        model.addAttribute("rList", rList);
+
+        // 로그 찍기(추후 찍은 로그를 통해 이 함수 호출이 끝났는지 파악하기 용이하다.)
+        log.info(this.getClass().getName() + ".noticeListUsingQueryDSL End!");
+
+        // 함수 처리가 끝나고 보여줄 HTML (Thymeleaf) 파일면
+        // templates/notice/noticeList.html
+        return "notice/noticeListJoin";
+    }
 }
