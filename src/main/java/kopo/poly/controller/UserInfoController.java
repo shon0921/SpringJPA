6dc4db55c8930cb459acc865dd900a9c47d500bc
shon0921 <43308838+shon0921@users.noreply.github.com>
2025-03-13 23:41:46 +0900

    Update UserInfoController.java

--- src/main/java/kopo/poly/controller/UserInfoController.java
+++ src/main/java/kopo/poly/controller/UserInfoController.java
@@ -1,4 +1,185 @@
 package kopo.poly.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import kopo.poly.dto.MsgDTO;
+import kopo.poly.dto.UserInfoDTO;
+import kopo.poly.service.IUserInfoService;
+import kopo.poly.util.CmmUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Optional;
+
+@Slf4j
+@RequestMapping(value = "/user")
+@RequiredArgsConstructor
+@Controller
 public class UserInfoController {
+
+    // @RequiredArgsConstructor 를 통해 메모리에 올라간 서비스 객체를 Controller에서 사용할 수 있게 주입함
+    private final IUserInfoService userInfoService;
+
+    @GetMapping(value = "userRegForm")
+    public String userRegForm() {
+        log.info("{}.user/userRegForm Start!", this.getClass().getName());
+
+        log.info("{}.user/userRegForm End!", this.getClass().getName());
+
+        return "user/userRegForm";
+    }
+
+    /*
+        회원 가입 전 아이디 중복체크하기(Ajax를 통해 입력한 아이디 정보 받음)
+     */
+    @ResponseBody
+    @PostMapping("getUserIdExists")
+    public UserInfoDTO getUserExists(HttpServletRequest request) throws Exception {
+
+        log.info("{}.geUserExists Start!", this.getClass().getName());
+
+        String userId = CmmUtil.nvl(request.getParameter("userId")); // 회원아이디
+
+        log.info("userId : {}", userId);
+
+        // Builder 통한 값 저장
+        UserInfoDTO pDTO = UserInfoDTO.builder().userId(userId).build();
+
+        // 회원아이디를 통해 중복된 아이디인지 조회
+        UserInfoDTO rDTO = Optional.ofNullable(userInfoService.getUserIdExists(pDTO))
+                .orElseGet(() -> UserInfoDTO.builder().build());
+
+        log.info("{}.geUserExists End", this.getClass().getName());
+
+        return rDTO;
+    }
+
+    /*
+        회원가입 로직 처리
+     */
+    @ResponseBody
+    @PostMapping(value = "insertUserInfo")
+    public MsgDTO insertUserInfo(HttpServletRequest request) throws Exception {
+
+        log.info("{}.insertUserInfo start!", this.getClass().getName());
+
+        String msg; // 회원가입 결과에 대한 메시지를 전달한 변수
+
+        String userId = CmmUtil.nvl(request.getParameter("userId")); // 아이디
+        String userName = CmmUtil.nvl(request.getParameter("userName")); // 이름
+        String password = CmmUtil.nvl(request.getParameter("password")); // 비밀번호
+        String email = CmmUtil.nvl(request.getParameter("email")); // 이메일
+        String addr1 = CmmUtil.nvl(request.getParameter("addr1")); // 주소
+        String addr2 = CmmUtil.nvl(request.getParameter("addr2")); // 상세주소
+
+        log.info("userId : {}, userName : {}, password : {}, email : {}, addr1 : {}, addr2 : {}",
+                userId, userName, password, email, addr1, addr2);
+
+        UserInfoDTO pDTO = UserInfoDTO.builder()
+                .userId(userId)
+                .userName(userName)
+                .password(EncryptUtil.encHashSHA256(password))      // 해시암호화 (복호화 불가능)
+                .email(EncryptUtil.encAES128CBC(email))     // AES128-CBC 암호화   (복호화 가능)
+                .addr1(addr1)
+                .addr2(addr2)
+                .regId(userId)
+                .chgId(userId)
+                .build();
+
+        int res = userInfoService.insertUserInfo(pDTO);
+
+        log.info("회원가입 결과(res) : {}", res);
+
+        if (res == 1) {
+            msg = "회원가입되었습니다.";
+        } else if (res == 2) {
+            msg = "이미 가입된 아이디입니다.";
+        } else {
+            msg = "오류로 인해 회원가입이 실패하였습니다.";
+        }
+
+        MsgDTO dto = MsgDTO.builder().result(res).msg(msg).build();
+
+        log.info("{}.insertUserInfo End!", this.getClass().getName());
+
+        return dto;
+    }
+
+    /*
+        로그인을 위한 입력 화면으로 이동
+     */
+    @GetMapping(value = "login")
+    public String login() {
+        log.info("{}.user/login Start!", this.getClass().getName());
+
+        log.info("{}.user/login End", this.getClass().getName());
+
+        return "user/login";
+    }
+
+    /*
+        로그인 처리 및 결과 알려주는 화면으로 이동
+     */
+    public MsgDTO loginProc(HttpServletRequest request, HttpSession session) throws Exception {
+
+        log.info("{}.loginProc Start!", this.getClass().getName());
+
+        String msg; // 로그인 결과에 대한 메시지를 전달할 변수
+
+        String user_id = CmmUtil.nvl(request.getParameter("user_id"));  // 아이디
+        String password = CmmUtil.nvl(request.getParameter("password"));    // 비밀번호
+
+        log.info("user_id : {}, password : {}", user_id, password);
+
+        UserInfoDTO pDTO = UserInfoDTO.builder()
+                .userId(user_id)
+                .password(EncryptUtil.encHashSHA256(password)).build();
+
+        int res = userInfoService.insertUserInfo(pDTO);
+
+        log.info("res : {}", res);
+
+        if (res == 1) { // 로그인 성공
+            msg = "로그인이 성공했습니다.";
+            session.setAttribute("SS_USER_ID", user_id);
+        } else {
+            msg = "아이디와 비밀번호가 올바르지 않습니다.";
+        }
+
+        // 결과 메시지 전달하기
+        MsgDTO dto = MsgDTO.builder().result(res).msg(msg).build();
+        log.info("{}.loginProc End!", this.getClass().getName());
+
+        return dto;
+    }
+
+    @GetMapping(value = "loginSuccess")
+    public String loginSuccess() {
+        log.info("{}.user/loginSuccess Start!", this.getClass().getName());
+
+        log.info("{}.user/loginSuccess End!", this.getClass().getName());
+
+        return "user/loginSuccess";
+    }
+
+    @ResponseBody
+    @PostMapping(value = "logout")
+    public MsgDTO logout(HttpSession session) {
+
+        log.info("{}.logout Start!", this.getClass().getName());
+
+        session.setAttribute("SS_USER_ID","");
+        session.removeAttribute("SS_USER_ID");
+
+        MsgDTO dto = MsgDTO.builder().result(1).msg("로그아웃하였습니다").build();
+
+        log.info("{}.logout End!", this.getClass().getName());
+
+        return dto;
+    }
 }
